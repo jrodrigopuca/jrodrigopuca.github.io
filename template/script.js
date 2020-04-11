61cd93dfe738db2f5a910c1888f907be8d6f0102
J.A. Rodrigo Puca <r@geeks.ms>
2020-04-10 22:59:21 -0300

    4/10: modal

--- template/script.js
+++ template/script.js
@@ -2,7 +2,8 @@
 const video = document.getElementById('video');
 let pipButton = document.getElementById('pipButton');
 let float = document.getElementsByClassName('float-block')[0];
-
+let modal = document.getElementById('modal');
+let modalContent = document.getElementById('modalContent');
 
 const hidden = !document.pictureInPictureEnabled || video.disablePictureInPicture;
 if (hidden) float.style.visibility = "hidden";
@@ -21,6 +22,28 @@ pipButton.addEventListener('click', () => {
             });
     }
 });
+
+const afterClick=()=>{
+    modal.style.display="block";
+}
+
+let buttons = document.getElementsByClassName('btn-green');
+Object.entries(buttons).map((btn)=>{
+    btn[1].addEventListener("mouseup",afterClick, false)
+})
+
+
+window.onclick = function(event){
+    if (event.target == modal){
+        modal.style.display = "none";
+    }
+}
+
+modalContent.onclick = function(event){
+    modal.style.display="none";
+}
+
+
 
 
 /*
